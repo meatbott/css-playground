a964701eb8a7be835fd0fb7bb7a5c74cf7397b04
Nathan Heck <36094454+MeatBot99@users.noreply.github.com>
2021-07-27 08:39:27 -0600

    Basic setup

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
 //App.js//
+/*The rough idea is that I can create different examples of CSS manipulation and display them as a grid
+so that I might reference them later. I have found that sometimes I forget how I did certain things and
+googling it proves difficult occasionally so this will be a more solid reference. That is the idea anyway.*/
