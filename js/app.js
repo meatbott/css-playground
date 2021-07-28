cf269db89f412ebf31df4f11063dd5f70ef8ee0c
Nathan Heck <36094454+MeatBot99@users.noreply.github.com>
2021-07-28 10:57:38 -0600

    User Stories

--- js/app.js
+++ js/app.js
@@ -1,4 +1,38 @@
 //App.js//
 /*The rough idea is that I can create different examples of CSS manipulation and display them as a grid
 so that I might reference them later. I have found that sometimes I forget how I did certain things and
-googling it proves difficult occasionally so this will be a more solid reference. That is the idea anyway.*/
+googling it proves difficult occasionally so this will be a more solid reference. That is the idea anyway.
+
+  Examples being animations, repeating/looping animations, gradients, changing fonts, movement of elements,
+  shape drawing/svgs, interactive elements/UX enhanement, color changing/flashing
+
+  Each "card" should feature something different and useful if not interesting.
+
+*/
+
+
+/*TODO: Use a master array to help with looping...or create adhoc array to hold objects 
+
+        Implement internal storage system-Objects in the data.js file
+          components
+            Title
+            name
+            content- sub divs/elements to manipulate
+            background color/image
+            order/date/time of creation
+            buttons
+
+        Create functions that pull from aforementioned Objects to generate DOM elements
+          generic inputs
+          Maybe there is a better way to do this but I am not sure right now.
+
+        Ulitlize local storage for customization/practice using local storage
+
+        Think about types of css class archetypes instead of individual classes, broader instead of narrower, as
+        applicable. This is probably more useful when doing a project that is not focused on CSS but I want to
+        still try to make it work.
+
+        I should be able to ideally make a new object in data.js file by writing in the file that will populate
+        a new "card". If I was really good I could make a new object by typing into a text field in the actual
+        webpage. This is for later but it is good to think about.
+*/
